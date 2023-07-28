1d1f3dcf9f262a74591539d37c654a6c00ab242d
Olli Paakkunainen <olli@hamina.com>
2023-07-28 12:40:35 +0300

    Add logging to examples

--- examples/print_stations.rs
+++ examples/print_stations.rs
@@ -1,6 +1,14 @@
+use log::LevelFilter;
 use netlink_wi::NlSocket;
+use simple_logger::SimpleLogger;
 
 fn main() {
+    SimpleLogger::new()
+        .with_level(LevelFilter::Debug)
+        .with_module_level("neli", LevelFilter::Info)
+        .init()
+        .unwrap();
+
     let mut socket = NlSocket::connect().unwrap();
     let interfaces = socket.list_interfaces().unwrap();
     for interface in interfaces {
--- examples/print_wiphys.rs
+++ examples/print_wiphys.rs
@@ -1,7 +1,15 @@
 /// Print physical wireless device information.
+use log::LevelFilter;
 use netlink_wi::NlSocket;
+use simple_logger::SimpleLogger;
 
 fn main() {
+    SimpleLogger::new()
+        .with_level(LevelFilter::Debug)
+        .with_module_level("neli", LevelFilter::Info)
+        .init()
+        .unwrap();
+
     let mut socket = NlSocket::connect().unwrap();
     let devices = socket.list_physical_devices().unwrap();
     for device in devices {
