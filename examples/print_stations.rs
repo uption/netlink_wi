845dd1336994089bbaa0e9b26b6c6c91a8274812
Olli Paakkunainen <olli@paakkunainen.fi>
2024-01-27 09:32:01 +0200

    Remove redundant redefinition from print stations example

--- examples/print_stations.rs
+++ examples/print_stations.rs
@@ -12,7 +12,6 @@ fn main() {
     let socket = NlSocket::connect().unwrap();
     let interfaces = socket.list_interfaces().unwrap();
     for interface in interfaces {
-        let interface = interface;
         let stations = socket.list_stations(interface.interface_index).unwrap();
         for station in stations {
             println!("{station:#?}");
